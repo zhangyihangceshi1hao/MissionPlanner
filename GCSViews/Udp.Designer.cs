ce35fff341b717456c645307d6fc4ccbd0896435
zyh <3045883533@qq.com>
2025-06-12 13:10:04 +0800

    feature(#2025612):调试已通

---
+++ GCSViews/Udp.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MissionPlanner.GCSViews
+{
+    partial class Udp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.myButton2 = new MissionPlanner.Controls.MyButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "系统IP地址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 28);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 33);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 28);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "10002";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 41);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "端口";
+            // 
+            // myButton2
+            // 
+            this.myButton2.Location = new System.Drawing.Point(559, 33);
+            this.myButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.myButton2.Name = "myButton2";
+            this.myButton2.Size = new System.Drawing.Size(112, 34);
+            this.myButton2.TabIndex = 9;
+            this.myButton2.Text = "连接";
+            this.myButton2.TextColorNotEnabled = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(87)))), ((int)(((byte)(4)))));
+            this.myButton2.UseVisualStyleBackColor = true;
+            this.myButton2.Click += new System.EventHandler(this.myButton2_Click);
+            // 
+            // Udp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 104);
+            this.Controls.Add(this.myButton2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Udp";
+            this.Text = "数据转发";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private Controls.MyButton myButton2;
+    }
+}
---
+++ GCSViews/Udp.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Scripting.Utils;
+using MissionPlanner.ArduPilot;
+using MissionPlanner.Swarm.Sequence;
+using MissionPlanner.Utilities;
+using netDxf.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using static Community.CsharpSqlite.Sqlite3;
+using static IronPython.Modules._ast;
+using static MAVLink;
+using static MissionPlanner.GCSViews.Udp;
+using DateTime = System.DateTime;
+
+namespace MissionPlanner.GCSViews
+{
+    public partial class Udp : Form
+    {
+        public Udp()
+        {
+            InitializeComponent();
+            countdown.Elapsed += sendMassage;
+        }
+
+       
+        Thread receive;
+
+        System.Timers.Timer countdown = new System.Timers.Timer { Interval = 1000, AutoReset = true };
+
+     
+
+        private void myButton2_Click(object sender, EventArgs e)
+        {
+            if (myButton2.Text == "连接")
+            {
+
+                countdown.Start();
+
+
+                myButton2.Text = "断开连接";
+            }
+            else
+            {
+              
+                myButton2.Text = "连接";
+                countdown.Close();
+            }
+        }
+
+
+
+
+
+
+
+        private async void receivemessage()
+        {
+            while (true)
+            {
+                try
+                {
+                  
+
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        // 定义无人机状态类
+        public class DroneStatus
+        {
+            public string UAVId { get; set; }
+            public string Longitude { get; set; }
+            public string Latitude { get; set; }
+            public string Altitude { get; set; }
+            public string RelativeHeight { get; set; }
+            public string AirSpeed { get; set; }
+            public string Groundspeed { get; set; }
+            public string Pitch { get; set; }
+            public string Roll { get; set; }
+            public string Yaw { get; set; }
+            public string Batterylevel { get; set; }
+            public string Linkqualitygcs { get; set; }
+            public string Satcount { get; set; }
+        }
+
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
+
+        /// <summary>
+        /// 发送无人机基本信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void sendMassage(object sender, ElapsedEventArgs e)
+        {
+           
+                // 创建要发送的数据
+                var droneData = new DroneStatus
+                {
+                    UAVId = "1",
+                    Longitude = "40.1234567",
+                    Latitude = "85.1234567",
+                    Altitude = "150",
+                    RelativeHeight = "10",
+                    AirSpeed = "10",
+                    Groundspeed = "20",
+                    Pitch = "30",
+                    Roll = "15",
+                    Yaw = "20",
+                    Batterylevel = "15.23",
+                    Linkqualitygcs = "100",
+                    Satcount = "12"
+                };
+
+                // 将对象序列化为 JSON 字符串
+                string json = JsonConvert.SerializeObject(droneData);
+
+                // 构建 HTTP 请求内容
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // 创建 HttpClient 并发送 POST 请求
+                
+                    try
+                    {
+                        var response = await _httpClient.PostAsync("http://localhost:5030/uav_status/api/data", content);
+
+                        Console.WriteLine($"HTTP Status Code: {response.StatusCode}");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            Console.WriteLine("Response Body: " + responseBody);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: " + await response.Content.ReadAsStringAsync());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("发送失败：" + ex.Message);
+                    }
+                
+            }
+        
+
+
+
+    }
+}
